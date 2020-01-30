b961cf628242d8266956eaa4bfab211e48fc317b
Nestor Escoto <nescoto@Nestors-MacBook-Pro.local>
2020-01-30 15:02:03 -0500

    Agregando Calc

--- calculadora.go
+++ calculadora.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 )
 
-type calc struct{}
+type Calc struct{}
 
-func (calc) operate(entrada string, operador string) int {
+func (Calc) Operate(entrada string, operador string) int {
 	entradaLimpia := strings.Split(entrada, operador)
 	operador1 := parsear(entradaLimpia[0])
 	operador2 := parsear(entradaLimpia[1])
